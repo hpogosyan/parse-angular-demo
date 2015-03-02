0d3eed61e4809282f9f3108ef089dfda37d1b094
John Pogosyan <johnpogosyan@gmail.com>
2015-03-01 17:11:39 -0800

    Changed App Keys

--- client/app/scripts/services/ParseServices.js
+++ client/app/scripts/services/ParseServices.js
@@ -2,7 +2,7 @@ angular.module('ParseServices', [])
 .factory('ParseSDK', function() {
 
   // pro-tip: swap these keys out for PROD keys automatically on deploy using grunt-replace
-  Parse.initialize("oInJV2JlcXR96EN0SQjee0n5FlRUAd0joyFXReve", "OOZa6rsbkIiRWyiNlmHVMgMIHAyTqPdHRg7N1PxO");
+  Parse.initialize("WCK0sOqYGdPd8CshG7wAscvqaCLqKyiSM9vPZAqF", "px0lu4O1jAHlAeFmtgWEQERKvmUdRLE46dh5idR9");
 
   // FACEBOOK init
   window.fbPromise.then(function() {
@@ -10,7 +10,7 @@ angular.module('ParseServices', [])
     Parse.FacebookUtils.init({
 
       // pro-tip: swap App ID out for PROD App ID automatically on deploy using grunt-replace
-      appId: 481650395275919, // Facebook App ID
+      appId: 1602397079974660, // Facebook App ID
       channelUrl: 'http://brandid.github.io/parse-angular-demo/channel.html', // Channel File
       cookie: true, // enable cookies to allow Parse to access the session
       xfbml: true, // parse XFBML
